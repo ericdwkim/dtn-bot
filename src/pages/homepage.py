6207f75e8f80e9fd2332b667f5ed6f9264734202
ericdwkim <ericdwkim.io@gmail.com>
2023-06-01 15:34:38 -0500

    Constructor for HomePage added.

--- src/pages/homepage.py
+++ src/pages/homepage.py
@@ -1,3 +1,9 @@
 from selenium.webdriver.common.by import By
 
 class HomePage:
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    # def do_something(self):
+    #     # interacting with elements, etc.
+    #     pass
