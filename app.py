fb96fe7aae247b233edc49e93f0d9b618dfc6105
ericdwkim <ericdwkim.io@gmail.com>
2023-05-31 19:33:31 -0500

    Login and switch to DC tab

--- app.py
+++ app.py
@@ -14,3 +14,15 @@ options = webdriver.ChromeOptions()
 # options.add_argument('--headless=new')
 # options.add_argument('--start-maximized')
 driver = uc.Chrome(use_subprocess=True, version_main=113, options=options)
+driver.get(dtn_url)
+
+# Login
+usrname_txtbox_selector = driver.find_element(By.CSS_SELECTOR, '#username').click()
+usrname_txtbox_selector.send_keys(username)
+pw_txtbox_selector = driver.find_element(By.CSS_SELECTOR, '.loginContent > form:nth-child(3) > div:nth-child(2) > input:nth-child(1)').click()
+usrname_txtbox_selector.send_keys(password)
+login_btn_selector = driver.find_element(By.CSS_SELECTOR, '.confirmButton').click()
+
+# DataConnect tab
+dc_tab_wait = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#header > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)')))
+dc_tab_selector = driver.find_element(By.CSS_SELECTOR, '#header > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)').click()
