581cfcce7808387e36ea2742c0bf8ebe7576ea53
ericdwkim <ericdwkim.io@gmail.com>
2023-06-01 15:32:54 -0500

    Env vars in LoginPage constructor. Login function completed.

--- src/pages/basepage.py
+++ src/pages/basepage.py
@@ -9,42 +9,25 @@ dtn_url = os.getenv('DTN_URL')
 class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
+        self.url = dtn_url
+        self.password = password
+        self.username = username
 
 class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.url = dtn_url
-        self.password = password
-        self.username = username
 
     def visit(self):
         self.driver.get(self.url)
 
     def login(self, username, password):
-        # implementation of the login
-        pass
-
+        self.username = username
+        self.password = password
 
 class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.url = dtn_url
 
-    def visit(self):
-        self.driver.get(self.url)
-
-    def do_something(self):
-        # interacting with elements, etc.
-        pass
-
-
-# usage
-driver = webdriver.Chrome()
-home = HomePage(driver)
-home.visit()
-# do more with the home page
-
-login = LoginPage(driver)
-login.visit()
-login.login('username', 'password')
-# do more with the login page
+    # def do_something(self):
+    #     # interacting with elements, etc.
+    #     pass
