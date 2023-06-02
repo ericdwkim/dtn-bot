1caac9f1172257cc6236c4adc2e3b990538571b5
ericdwkim <ericdwkim.io@gmail.com>
2023-06-02 18:58:18 -0500

    Implemented WebDriverWait and find_element functions to parent class.

--- src/pages/basepage.py
+++ src/pages/basepage.py
@@ -1,6 +1,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Environmental variables
 username = os.getenv('DTN_EMAIL_ADDRESS')
@@ -12,17 +14,24 @@ class BasePage(object):
         self.driver = driver
         self.url = dtn_url
 
-    # TODO: Abstracted function for WebDriverWait use cases
-    # def wait_for_page_to_load(self, timeout=10):
-    #     WebDriverWait(self.driver, timeout).until(
-    #         lambda driver: driver.execute_script("return document.readyState") == "complete"
-    #     )
+    def wait_for_page_to_load(self, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            lambda driver: driver.execute_script("return document.readyState") == "complete"
+        )
 
-# def wait_for_element(self, locator, locator_type=By.ID, timeout=10):
-#     element = WebDriverWait(self.driver, timeout).until(
-#         EC.presence_of_element_located((locator_type, locator))
-#     )
-#     return element
+    def wait_for_element(self, locator, locator_type=By.CSS_SELECTOR, timeout=10):
+        element = WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located((locator_type, locator))
+        )
+        return element
+
+    def find_element_and_click(self, locator_type, locator):
+        element_selector = self.driver.find_element(locator_type, locator).click()
+        return element_selector
+
+    def find_element_and_click_and_send_keys(self, locator_type, locator, keys_to_send):
+        element_selector_clicked = find_element_and_click(locator_type, locator)
+        element_selector_clicked.send_keys(keys_to_send)
 
 class LoginPage(BasePage):
     def __init__(self, driver):
