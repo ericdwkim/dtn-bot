39174534122b48ed9c963c967dd88e95a3231878
ericdwkim <ericdwkim.io@gmail.com>
2023-06-03 19:46:18 -0500

    Timed out at wait_for_find_then_click of plus sign UI for No selection.

--- src/pages/dataconnectpage.py
+++ src/pages/dataconnectpage.py
@@ -19,22 +19,17 @@ class DataConnectPage(BasePage):
 
     def set_translated_filter(self):
 
-        # Translated filter header column
+        # Translated funnel header
         self.wait_for_find_then_click('th.sorting:nth-child(7) > button:nth-child(1) > span:nth-child(2)')
 
-        # Translated pop up widget's `No` filter
-        # iframe = self.driver.find_element(By.ID, 'LifeLine_iframe')
 
-        # iframe = self.driver.find_element(By.ID, 'cz-clean-room')
-        # iframe = self.driver.find_element(By.ID, 'cz_success_center_launcher_frame')
-        # iframe = self.driver.find_element(By.ID, 'cz_success_center_alert_flyout_frame')
-        # iframe = self.driver.find_element(By.ID, 'cz_success_center_frame')
-
-        self.driver.switch_to.frame("LifeLine_iframe")
-        # self.wait_for_find_then_click('li.ui-draggable:nth-child(1) > a:nth-child(2) > span:nth-child(1)')
-
-        # self.wait_for_element('li.ui-draggable:nth-child(1) > a:nth-child(2) > span:nth-child(1)')
-        self.find_element_and_click('li.ui-draggable:nth-child(1) > a:nth-child(2) > span:nth-child(1)')
+        # Switch to iframe
+        # self.driver.switch_to.frame('LifeLine_iframe')
+        self.driver.switch_to.frame('cz-clean-room')
+        # self.driver.switch_to.frame('LifeLine_iframe')
+        # self.driver.switch_to.frame('LifeLine_iframe')
+        # driver.switch_to.frame(driver.find_elements(By.TAG_NAME, "iframe")[0])
+        self.wait_for_find_then_click('li.ui-draggable:nth-child(1) > a:nth-child(2) > span:nth-child(1)')
 
         # self.find_element_and_click('ui-corner-all ui-icon ui-icon-plus', locator_type=By.CLASS_NAME)
 
