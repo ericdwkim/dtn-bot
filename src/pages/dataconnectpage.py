fa8bd9d36a1c5e78e35d66d5846b2826ea551ff7
ericdwkim <ericdwkim.io@gmail.com>
2023-06-04 02:56:11 -0500

    Switching to finding elements using Chrome instead of FF.

--- src/pages/dataconnectpage.py
+++ src/pages/dataconnectpage.py
@@ -35,7 +35,14 @@ class DataConnectPage(BasePage):
         # Wait for the span element to be clickable
 
         # no_plus_wait = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[8]/div[2]/div[2]/ul/li[1]/a/span')))
-        no_plus = self.driver.find_element(By.XPATH, '/html/body/div[8]/div[2]/div[2]/ul/li[1]/a/span').click()
+
+        no_plus_1 = self.driver.find_element('body')
+        no_plus_2 = self.driver.find_element('div:nth-child(15)')
+        no_plus_3 = self.driver.find_element('div.ui-multiselect.ui-helper-clearfix.ui-widget.ui-dialog-content.ui-widget-content')
+        no_plus_4 = self.driver.find_element('div.available.right-column')
+        no_plus_5 = self.driver.find_element('ul')
+        no_plus_6 = self.driver.find_element('li:nth-child(1)')
+        no_plus_6.click()
 
         # `No` plus icon
         # self.find_element_and_click('li.ui-draggable:nth-child(1) > a:nth-child(2) > span:nth-child(1)')
