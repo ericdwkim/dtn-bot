59d17a81e358a0a670ebf54a7e9f13a004471e6e
ericdwkim <ericdwkim.io@gmail.com>
2023-06-05 16:56:02 -0500

    Test - Shadow DOM theory

--- src/pages/dataconnectpage.py
+++ src/pages/dataconnectpage.py
@@ -20,7 +20,7 @@ class DataConnectPage(BasePage):
         # Translated funnel header
         self.retry_wait_find_then_click("th.sorting:nth-child(7) > button:nth-child(1) > span:nth-child(2)")
 
-        # No draggable bar; double click to set
+        # Test - if no drag bar is found, we can rule out shadow DOM theory
         no_drag_bar_wait = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[8]/div[2]/div[2]/ul/li[1][contains(., 'No')]")))
         no_drag_bar= self.driver.find_element(By.XPATH, "/html/body/div[8]/div[2]/div[2]/ul/li[1][contains(., 'No')]")
 
