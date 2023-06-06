133efcd0b9ba2ed1efb1bef1ace9740b68eabdc1
ericdwkim <ericdwkim.io@gmail.com>
2023-06-05 20:01:04 -0500

    Additional helper functions for double click use cases

--- src/pages/basepage.py
+++ src/pages/basepage.py
@@ -4,9 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+
 class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
+        self.action = ActionChains(self.driver)
 
     def find_element_and_click(self, locator ,locator_type=By.CSS_SELECTOR):
         element_selector = self.driver.find_element(locator_type, locator)
@@ -16,6 +19,12 @@ class BasePage(object):
     def find_element_and_click_and_send_keys(self, locator, keys_to_send):
         element_selector_clicked = self.find_element_and_click(locator)
         element_selector_clicked.send_keys(keys_to_send)
+
+
+    def find_element_and_double_click(self, locator, locator_type=By.CSS_SELECTOR):
+        element = self.driver.find_element(locator_type, locator)
+        self.action.double_click(element)
+
 
     def wait_for_page_to_load(self, timeout=10):
         WebDriverWait(self.driver, timeout).until(
@@ -38,6 +47,11 @@ class BasePage(object):
         element_selector_clicked = self.find_element_and_click(locator)
         return element_selector_clicked
 
+    def wait_for_find_then_double_click(self, locator):
+        self.wait_for_element(locator)
+        element_selector_double_clicked = self.find_element_and_double_click(locator)
+        return element_selector_double_clicked
+
     def wait_for_find_click_then_send_keys(self, locator, keys_to_send):
         element_selector_clicked = self.wait_for_find_then_click(locator)
         # self.find_element_and_click_and_send_keys(locator, keys_to_send)
@@ -48,6 +62,20 @@ class BasePage(object):
         while retries < max_retries:
             try:
                 element = self.wait_for_find_then_click(locator)
+                break  # Break out of the loop if element is found and clicked successfully
+            except (NoSuchElementException, TimeoutException):
+                print(f"Element not found. Retrying... (Attempt {retries+1}/{max_retries})")
+                retries += 1
+                time.sleep(retry_delay)  # Delay before retrying
+        else:
+            # Executed if the loop completes without encountering a break statement (i.e., max_retries reached)
+            print("Maximum number of retries reached. Element not found.")
+
+    def retry_wait_find_then_double_click(self, locator, max_retries=5, retry_delay=1):
+        retries = 0
+        while retries < max_retries:
+            try:
+                element = self.wait_for_find_then_double_click(locator)
                 break  # Break out of the loop if element is found and clicked successfully
             except (NoSuchElementException, TimeoutException):
                 print(f"Element not found. Retrying... (Attempt {retries+1}/{max_retries})")
--- src/pages/dataconnectpage.py
+++ src/pages/dataconnectpage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 class DataConnectPage(BasePage):
     def __init__(self, driver):
@@ -21,26 +22,28 @@ class DataConnectPage(BasePage):
         self.retry_wait_find_then_click("th.sorting:nth-child(7) > button:nth-child(1) > span:nth-child(2)")
 
         # Set Translated to `No`
-        no_drag_bar_wait = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[8]/div[2]/div[2]/ul/li[1][contains(., 'No')]")))
-        no_drag_bar= self.driver.find_element(By.XPATH, "/html/body/div[8]/div[2]/div[2]/ul/li[1][contains(., 'No')]")
-        action.double_click(no_drag_bar)
+        self.retry_wait_find_then_double_click(By.XPATH, "/html/body/div[8]/div[2]/div[2]/ul/li[1][contains(., 'No')]")
         print("No drag bar double clicked!")
+
+        # Wait to see if UI updates
+        time.sleep(10)
+
 
         # Click `filter` on widget to refresh current page to validate if Translated is all Nos
         # filter_btn_wait = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[10]/div[3]/div/button[1]/span")))
-        filter_btn= self.driver.find_element(By.XPATH, "/html/body/div[10]/div[3]/div/button[1]/span")
+        # filter_btn= self.driver.find_element(By.XPATH, "/html/body/div[10]/div[3]/div/button[1]/span")
 
         # Check if the element is displayed
-        if filter_btn.is_displayed():
-            print("Element is displayed")
-        else:
-            print("Element is not displayed")
-
-        # Check if the element is enabled
-        if filter_btn.is_enabled():
-            print("Element is enabled")
-        else:
-            print("Element is disabled")
+        # if filter_btn.is_displayed():
+        #     print("Element is displayed")
+        # else:
+        #     print("Element is not displayed")
+        #
+        # # Check if the element is enabled
+        # if filter_btn.is_enabled():
+        #     print("Element is enabled")
+        # else:
+        #     print("Element is disabled")
 
 
         # self.wait_for_element_clickable(By.XPATH, "/html/body/div[10]/div[3]/div/button[1]")
