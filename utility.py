3469845acf7c65f6db21654c298aacbae480c34a
ericdwkim <ericdwkim.io@gmail.com>
2023-06-07 16:55:16 -0500

    Import ChromeDriverManager to use LATEST chromedriver version. Version 114.

--- utility.py
+++ utility.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 import undetected_chromedriver as uc
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def setup_driver():
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless=new')
     options.add_argument('--start-maximized')
-    driver = uc.Chrome(use_subprocess=True, version_main=113, options=options)
+    # driver = uc.Chrome(use_subprocess=True, version_main=113, options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     return driver
 
 def teardown_driver(driver):
