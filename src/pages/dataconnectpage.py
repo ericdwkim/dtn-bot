a1c84c4b190fb6b63517abc407921f7d5bc94a81
ericdwkim <ericdwkim.io@gmail.com>
2023-06-08 17:36:57 -0500

    Test - set_translated_filter refactored.

--- src/pages/dataconnectpage.py
+++ src/pages/dataconnectpage.py
@@ -93,5 +93,5 @@ class DataConnectPage(BasePage):
     def switch_tab_and_apply_filters(self):
         self.switch_tab()
         self.set_date_filter()
-        self.set_translated_filter()
+        self.set_translated_filter() # Test - Refactored function without mappings
         # self.set_group_filter_to_invoice()
