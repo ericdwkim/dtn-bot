c434226c1054b6440645c8846470b330d63810e1
ericdwkim <ericdwkim.io@gmail.com>
2023-06-10 21:02:02 -0400

    Test - presence_of_all_elements_located also returns list of WebElements.

--- src/pages/basepage.py
+++ src/pages/basepage.py
@@ -20,7 +20,7 @@ class BasePage(object):
             else:
                 print(f'source_element: {source_element} | target_element: {target_element}')
             source_element_clickable = self.wait_for_element_clickable(src_locator, locator_type)
-            target_elements_visible = self.wait_for_visibility_of_elements(target_locator, locator_type) # TODO: still returning false 
+            target_elements_visible = self.wait_for_visibility_of_elements(target_locator, locator_type) # TODO: still returning false
             return source_element, source_element_clickable, target_element, target_elements_visible
         except Exception as e:
             print(
@@ -127,7 +127,7 @@ class BasePage(object):
 
         try:
             WebDriverWait(self.driver, timeout).until(
-                EC.visibility_of_all_elements_located((locator_type, locator))
+                EC.presence_of_all_elements_located((locator_type, locator))
             )
             return True
         except (NoSuchElementException, TimeoutException):
