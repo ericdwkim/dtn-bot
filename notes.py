b064b3e2269c7ec21850022631791edb157a8876
ericdwkim <ericdwkim.io@gmail.com>
2023-06-13 15:04:54 -0400

    Migrated notes.

---
+++ notes.py
@@ -0,0 +1,23 @@
+ """
+    <ul class="available connected-list" style="height: 172px;">
+        <li class="ui-state-default ui-element ui-draggable" title="Credit Card">
+        <span class="ui-helper-hidden">
+        </span>Credit Card<a href="#" class="action">
+        <span class="ui-corner-all ui-icon ui-icon-plus">
+        </span>
+        </a>
+        </li>
+    <li class="ui-state-default ui-element ui-draggable" title="Draft Notice">
+    <span class="ui-helper-hidden"></span>Draft Notice<a href="#" class="action">
+    <span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li><li class="ui-state-default ui-element ui-draggable" title="Invoice">
+    <span class="ui-helper-hidden"></span>Invoice<a href="#" class="action"><span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li>
+    <li class="ui-state-default ui-element ui-draggable" title="Price"><span class="ui-helper-hidden"></span>Price<a href="#" class="action">
+    <span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li><li class="ui-state-default ui-element ui-draggable" title="Rack Report">
+    <span class="ui-helper-hidden"></span>Rack Report<a href="#" class="action"><span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li></ul>
+    
+    @dev: above is the full copied element of the unordered list for *all* existing draggable bars for the `Group` filter
+    1) get XPATH syntax down for this ul element so that it returns a <List>WebElements
+                //ul[@class='available connected-list']
+    2) access desired idx from list (Credit Card, Draft Notice, Invoice, Price, Rack Report)
+    3) this will allow this new helper fn to be reusable for all `Group` filter setting use cases and NOT just for Invoice
+    """
--- src/pages/basepage.py
+++ src/pages/basepage.py
@@ -47,29 +47,7 @@ class BasePage(object):
         return source_element, source_element_clickable, target_element, target_elements_present
 
     # TODO: src_locator specific for `Invoice` draggable bar; need to abstract. See below
-    """
-    <ul class="available connected-list" style="height: 172px;">
-        <li class="ui-state-default ui-element ui-draggable" title="Credit Card">
-        <span class="ui-helper-hidden">
-        </span>Credit Card<a href="#" class="action">
-        <span class="ui-corner-all ui-icon ui-icon-plus">
-        </span>
-        </a>
-        </li>
-    <li class="ui-state-default ui-element ui-draggable" title="Draft Notice">
-    <span class="ui-helper-hidden"></span>Draft Notice<a href="#" class="action">
-    <span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li><li class="ui-state-default ui-element ui-draggable" title="Invoice">
-    <span class="ui-helper-hidden"></span>Invoice<a href="#" class="action"><span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li>
-    <li class="ui-state-default ui-element ui-draggable" title="Price"><span class="ui-helper-hidden"></span>Price<a href="#" class="action">
-    <span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li><li class="ui-state-default ui-element ui-draggable" title="Rack Report">
-    <span class="ui-helper-hidden"></span>Rack Report<a href="#" class="action"><span class="ui-corner-all ui-icon ui-icon-plus"></span></a></li></ul>
-    
-    @dev: above is the full copied element of the unordered list for *all* existing draggable bars for the `Group` filter
-    1) get XPATH syntax down for this ul element so that it returns a <List>WebElements
-                //ul[@class='available connected-list']
-    2) access desired idx from list (Credit Card, Draft Notice, Invoice, Price, Rack Report)
-    3) this will allow this new helper fn to be reusable for all `Group` filter setting use cases and NOT just for Invoice
-    """
+
 
     def find_and_wait_for_src_and_target_elems_to_be_present(
         self,
