26d4c7160d5986074417de49d8ec363abdb29991
ericdwkim <ericdwkim.io@gmail.com>
2023-06-21 12:14:35 -0500

    Refactored - extraction fn to return as many vars as possible.

--- pdf_handler.py
+++ pdf_handler.py
@@ -41,9 +41,9 @@ def extract_text_from_pdf_page(page):
 
     return text
 
+
 def extract_info_from_text(current_page_text, target_keywords):
     """Extract the specific information from a page"""
-    today = datetime.date.today().strftime('%m-%d-%y')
 
     # Extract EFT number
     eft_num_pattern = target_keywords[1]  # Assuming keyword is something like 'EFT-'
@@ -51,21 +51,28 @@ def extract_info_from_text(current_page_text, target_keywords):
     print(f'\nUsing eft_num_pattern: "{eft_num_pattern}"\nGetting eft_num_matches: {eft_num_matches}\n')
     if eft_num_matches:
         eft_num = eft_num_matches[0]
-
-    elif not eft_num_matches:
+    else:
         print(f"No matches for regular expression: {eft_num_pattern} in text:\n*****************************************************\n {current_page_text}\n*****************************************************\n")
-        return None, today, None
-    eft_num = eft_num_matches[0]
+        eft_num = None
 
     # Extract total_draft
     total_draft_keyword = target_keywords[0]
     total_draft_matches = re.findall(r'([\d,]+\.\d+)', current_page_text)
     print(f'\nUsing total_draft_keyword: "{total_draft_keyword}"\nGetting total_draft_matches: {total_draft_matches}\n')
-    if not total_draft_matches:
+    if total_draft_matches:
+        total_draft_amt = total_draft_matches[-1]
+    else:
         print(f"No matches for regular expression using keyword: {total_draft_keyword} in text:\n*****************************************************\n {current_page_text}\n*****************************************************\n")
-        return None, None, None
-    total_draft_amt = total_draft_matches[-1]
+        total_draft_amt = None
 
+    today = datetime.date.today().strftime('%m-%d-%y')
+
+    if eft_num is not None and total_draft_amt is None:
+        return eft_num, today, None
+    elif eft_num is None and total_draft_amt is not None:
+        return None, today, total_draft_amt
+    elif eft_num is None and total_draft_amt is None:
+        return None, today, None
 
     return eft_num, today, total_draft_amt
 
