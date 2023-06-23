37151f8281e4921cbadff09b4d1252d08af0351b
ericdwkim <ericdwkim.io@gmail.com>
2023-06-23 18:45:42 -0500

    Bump - Packaged handler modules

--- utils/pdf_handler.py
+++ utils/pdf_handler.py
@@ -6,6 +6,7 @@ import pikepdf
 import pdfplumber
 import io
 from post_processing import merge_rename_and_summate
+from extraction_handler import extract_text_from_pdf_page, extract_info_from_text
 
 # Invoices
 def rename_and_move_pdf(file_name, source_dir, target_dir):
@@ -23,58 +24,6 @@ def rename_and_move_pdf(file_name, source_dir, target_dir):
             shutil.move(source_file, destination_file)
             break  # If you're only expecting one such file, you can break the loop after the first one found
 
-# Take in pikepdf Pdf object, return extracted text
-def extract_text_from_pdf_page(page):
-    # Create a BytesIO buffer
-    pdf_stream = io.BytesIO()
-
-    # Write the page to the buffer
-    with pikepdf.Pdf.new() as pdf:
-        pdf.pages.append(page)
-        pdf.save(pdf_stream)
-
-    # Use pdfplumber to read the page from the buffer
-    pdf_stream.seek(0)
-    with pdfplumber.open(pdf_stream) as pdf:
-        page = pdf.pages[0]
-        text = page.extract_text()
-
-    return text
-
-
-def extract_info_from_text(current_page_text, target_keywords):
-    """Extract the specific information from a page"""
-
-    # Extract EFT number
-    eft_num_pattern = target_keywords[1]  # Assuming keyword is something like 'EFT-'
-    eft_num_matches = re.findall(eft_num_pattern, current_page_text)
-    print(f'\nUsing eft_num_pattern: "{eft_num_pattern}"\nGetting eft_num_matches: {eft_num_matches}\n')
-    if eft_num_matches:
-        eft_num = eft_num_matches[0]
-    else:
-        print(f"No matches for regular expression: {eft_num_pattern} in text:\n*****************************************************\n {current_page_text}\n*****************************************************\n")
-        eft_num = None
-
-    # Extract total_draft
-    # total_draft_keyword = target_keywords[0]
-    total_draft_matches = re.findall(r'([\d,]+\.\d+)', current_page_text)
-    # print(f'\nUsing total_draft_keyword: "{total_draft_keyword}"\nGetting total_draft_matches: {total_draft_matches}\n')
-    if total_draft_matches:
-        total_draft_amt = total_draft_matches[-1]
-    else:
-        print(f"No matches for regular expression using keyword: {total_draft_keyword} in text:\n*****************************************************\n {current_page_text}\n*****************************************************\n")
-        total_draft_amt = None
-
-    today = datetime.date.today().strftime('%m-%d-%y')
-
-    if eft_num is not None and total_draft_amt is None:
-        return eft_num, today, None
-    elif eft_num is None and total_draft_amt is not None:
-        return None, today, total_draft_amt
-    elif eft_num is None and total_draft_amt is None:
-        return None, today, None
-
-    return eft_num, today, total_draft_amt
 
 def get_full_path_to_dl_dir(download_dir, keyword_in_dl_file_name):
     full_path_to_downloaded_pdf = os.path.join(download_dir, f"{keyword_in_dl_file_name}.pdf")
