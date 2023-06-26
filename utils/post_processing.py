e3917704c775cf3fd83bb5424a885f35aedb099c
ericdwkim <ericdwkim.io@gmail.com>
2023-06-26 09:04:02 -0500

    Split up post processing driver function.

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -3,6 +3,7 @@ import re
 import datetime
 import pikepdf
 
+# TODO: 6-26-23 - test refactored driver function
 
 # TODO: need to fix this logic to 1) check if all appropriate multi AND single page pdfs are ready for post processing and located in temp_dir
 # TODO: this will be dependent on each original PDF. REMEMBER, this is specific to exxon 'TOTAL DISTRIBUTOR' calculation
@@ -20,50 +21,37 @@ def delete_pdf_files(directory_path):
             files_deleted = True
 
     return files_deleted
-def merge_rename_and_summate(temp_dir):
 
+def extract_pdf_data(temp_dir):
     today = datetime.date.today().strftime('%m-%d-%y')
-    # Get a list of all PDF files in the directory
     pdf_files = os.listdir(temp_dir)
-    # Create a list to store parsed information
     pdf_data = []
-    # Loop through each PDF file and extract information
     for pdf_file in pdf_files:
         if pdf_file.endswith('.pdf'):
             file_path = os.path.join(temp_dir, pdf_file)
-            # Extract information from the file name using regular expressions
             match = re.match(r'CCM-(\d+)-.*-(\d{1,3}(?:,\d{3})*\.\d+)\.pdf', pdf_file)
             if match:
                 regex_num = int(match.group(1))
                 total_credit_amt = float(match.group(2).replace(',', ''))
-                today = datetime.date.today().strftime('%m-%d-%y')
-
-                # Add extracted information to the list
                 pdf_data.append((regex_num, today, total_credit_amt, file_path))
-
-
-    # Sort the PDF data by regex_num
     pdf_data.sort(key=lambda x: x[0])
-
-
-    # Sum the total_credit_amt and round to second decimal place
     total_credit_amt_sum = round(sum(item[2] for item in pdf_data), 2)
+    return pdf_data, total_credit_amt_sum
 
-
-    # Merge the PDFs using pikepdf
+def merge_pdfs(pdf_data):
     merged_pdf = pikepdf.Pdf.new()
-
     for _, _, _, file_path in pdf_data:
         pdf = pikepdf.Pdf.open(file_path)
         merged_pdf.pages.extend(pdf.pages)
+    return merged_pdf
 
-    # Save the merged PDF with the new file name
-    new_file_name = f'CCM-{today}-{total_credit_amt_sum}.pdf'  # Use the first regex_num and today
+def save_merged_pdf(temp_dir, merged_pdf, total_credit_amt_sum):
+    today = datetime.date.today().strftime('%m-%d-%y')
+    new_file_name = f'CCM-{today}-{total_credit_amt_sum}.pdf'
     output_dir = temp_dir[:-5]
     output_path = os.path.join(output_dir, new_file_name)
     merged_pdf.save(output_path)
-
-
+    merged_pdf.close()
     if os.path.exists(output_path):
         print(f'PDFs have been merged, renamed, and moved to: {output_path}')
         temp_files_deleted = delete_pdf_files(temp_dir)
@@ -76,3 +64,7 @@ def merge_rename_and_summate(temp_dir):
 
     merged_pdf.close()
 
+def merge_rename_and_summate(temp_dir):
+    pdf_data, total_credit_amt_sum = extract_pdf_data(temp_dir)
+    merged_pdf = merge_pdfs(pdf_data)
+    save_merged_pdf(temp_dir, merged_pdf, total_credit_amt_sum)
