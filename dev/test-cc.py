9b18ce43ca7a2ed752266bfac8f9ae68cb0dd765
ericdwkim <ericdwkim.io@gmail.com>
2023-06-26 10:18:38 -0500

    Test - Split process_page into multi and single. Wrapper to separate multi and single page processing.

--- dev/test-cc.py
+++ dev/test-cc.py
@@ -29,6 +29,25 @@ company_name_to_subdir_full_path_mapping_credit_cards = {
 
 }
 
+# def process_exxon_ccm(current_pages, new_file_name, destination_dir, temp_dir, company_name, regex_num):
+#     if company_name != 'EXXONMOBIL':
+#         single_made_pdf_saved = create_and_save_pdf(current_pages, new_file_name, destination_dir)
+#     elif company_name == 'EXXONMOBIL':
+#         single_page_pdf_saved_in_temp = create_and_save_pdf(current_pages, new_file_name, temp_dir)
+#         if single_page_pdf_saved_in_temp:
+#             merge_rename_and_summate(temp_dir)
+#         else:
+#             print(f'Could not post process multi page pdfs')
+#     if company_name != 'EXXONMOBIL' and re.match(r'CMB-\d+', regex_num):
+#         multi_page_pdf_saved = create_and_save_pdf(current_pages, new_file_name, destination_dir)
+#     elif company_name == 'EXXONMOBIL' and re.match(r'CCM-\d+', regex_num):
+#         multi_page_pdf_saved_in_temp = create_and_save_pdf(current_pages, new_file_name, temp_dir)
+#         if multi_page_pdf_saved_in_temp:
+#             merge_rename_and_summate(temp_dir)
+#         else:
+#             print(f'Could not post process multi page pdfs')
+
+
 def create_and_save_pdf(pages, new_file_name, destination_dir):
     try:
         new_pdf = pikepdf.Pdf.new()
@@ -45,21 +64,20 @@ def get_new_file_name_cc(regex_num, today, total_credit_amt):
     # print(f'new_file_name: {new_file_name}')
     return new_file_name
 
-def process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, company_name_to_company_subdir_mapping):
-    for company_name, keywords in company_name_to_search_keyword_mapping.items():
 
+def process_multi_page(pdf, page_num, company_name_to_search_keyword_mapping, company_name_to_company_subdir_mapping):
+    for company_name, keywords in company_name_to_search_keyword_mapping.items():
 
         current_page_text = extract_text_from_pdf_page(pdf.pages[page_num])
         print(f'Processing page: {page_num + 1}')
         print(f'\n*****************************\n{current_page_text}\n*****************************\n')
 
         # Handles CCM, CMB multi page docs
-        if (re.search(r'CCM-\d+', current_page_text) or re.search(r'CMB-\d+', current_page_text)) and company_name in current_page_text and 'END MSG' not in current_page_text:
+        if (re.search(r'CCM-\d+', current_page_text) or re.search(r'CMB-\d+',
+                                                                  current_page_text)) and company_name in current_page_text and 'END MSG' not in current_page_text:
             # print(f'page_num: {page_num}')
-            current_pages =[]
+            current_pages = []
             current_page_texts = []
-
-
 
             while 'END MSG' not in current_page_text and page_num < len(pdf.pages):
                 current_pages.append(pdf.pages[page_num])
@@ -67,9 +85,7 @@ def process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, compa
                 current_page_texts.append(current_page_text)
                 print(f'current_page_texts: {current_page_texts}')
 
-
                 page_num += 1
-
 
                 if page_num >= len(pdf.pages):
                     break
@@ -92,11 +108,18 @@ def process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, compa
                 else:
                     print(f'Could not post process multi page pdfs')
 
+    return page_num
 
 
+def process_single_page(pdf, page_num, company_name_to_search_keyword_mapping, company_name_to_company_subdir_mapping):
+    for company_name, keywords in company_name_to_search_keyword_mapping.items():
+        current_page_text = extract_text_from_pdf_page(pdf.pages[page_num])
+        print(f'Processing page: {page_num + 1}')
+        print(f'\n*****************************\n{current_page_text}\n*****************************\n')
 
         # Handle single page CCM docs
-        if re.search(r'CCM-\d+', current_page_text) and company_name in current_page_text and 'END MSG' in current_page_text:
+        if re.search(r'CCM-\d+',
+                     current_page_text) and company_name in current_page_text and 'END MSG' in current_page_text:
             current_pages = [pdf.pages[page_num]]
             regex_num, today, total_amount = extract_info_from_text(current_page_text, keywords)
             new_file_name = get_new_file_name_cc(regex_num, today, total_amount)
@@ -106,6 +129,7 @@ def process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, compa
                 single_made_pdf_saved = create_and_save_pdf(current_pages, new_file_name, destination_dir)
 
             if company_name == 'EXXONMOBIL':
+                print(f'----------------------------------------------------------------')
                 single_page_pdf_saved_in_temp = create_and_save_pdf(current_pages, new_file_name, temp_dir)
                 if single_page_pdf_saved_in_temp:
                     merge_rename_and_summate(temp_dir)
@@ -118,7 +142,8 @@ def process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, compa
 
     return page_num
 
-def process_pdf_cc(filepath,  company_name_to_company_subdir_mapping,company_name_to_search_keyword_mapping):
+
+def process_multi_pages(filepath,  company_name_to_company_subdir_mapping,company_name_to_search_keyword_mapping):
     try:
 
         # Read original PDF from dls dir
@@ -128,7 +153,8 @@ def process_pdf_cc(filepath,  company_name_to_company_subdir_mapping,company_nam
             while page_num < len(pdf.pages):
                 print(f'page_num: {page_num + 1}')
                 # Process pages and update the page number at original PDF (macro) level
-                new_page_num = process_page_cc(pdf, page_num, company_name_to_search_keyword_mapping, company_name_to_company_subdir_mapping)
+                new_page_num = process_multi_page(pdf, page_num, company_name_to_search_keyword_mapping, company_name_to_company_subdir_mapping)
+
 
                 # if process_page has not incremented
                 # prevents one off issue
@@ -144,9 +170,50 @@ def process_pdf_cc(filepath,  company_name_to_company_subdir_mapping,company_nam
         print(f'An unexpected error occurred: {str(e)}')
         return False
 
-results = process_pdf_cc(file_path, company_name_to_subdir_full_path_mapping_credit_cards, company_name_to_search_keyword_mapping_credit_cards)
+
+def process_single_pages(filepath, company_name_to_company_subdir_mapping, company_name_to_search_keyword_mapping):
+    try:
+
+        # Read original PDF from dls dir
+        print(f'Processing file: {filepath}')
+        with pikepdf.open(filepath) as pdf:
+            page_num = 0  # Initialize page_num
+            while page_num < len(pdf.pages):
+                print(f'page_num: {page_num + 1}')
+
+                # Process pages and update the page number at original PDF (macro) level
+                new_page_num = process_single_page(pdf, page_num, company_name_to_search_keyword_mapping,
+                                                   company_name_to_company_subdir_mapping)
+
+                # if process_page has not incremented
+                # prevents one off issue
+                if new_page_num == page_num:
+                    page_num += 1
+                else:
+                    page_num = new_page_num
+
+            # If all pages processed without errors, return True
+            return True
+    except Exception as e:
+        # If any error occurred, print it and return False
+        print(f'An unexpected error occurred: {str(e)}')
+        return False
+
+
+def process_pdfs(filepath, company_name_to_company_subdir_mapping, company_name_to_search_keyword_mapping):
+    try:
+        single_pages_processed = process_single_pages(filepath, company_name_to_company_subdir_mapping, company_name_to_search_keyword_mapping)
+        if single_pages_processed:
+            print(f'successful test')
+            # multi_pages_processed = process_multi_pages(filepath, company_name_to_company_subdir_mapping, company_name_to_search_keyword_mapping)
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
+
+
+results = process_pdfs(file_path, company_name_to_subdir_full_path_mapping_credit_cards, company_name_to_search_keyword_mapping_credit_cards)
 if results:
     print('Finished')
+
 
 # TODO
 """
