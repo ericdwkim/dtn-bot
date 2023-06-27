b447200f14b8370f16e79a7af553477934f0ca4b
ericdwkim <ericdwkim.io@gmail.com>
2023-06-27 08:31:57 -0500

    Refactor - Handling and manipulating pdf data in memory without writing to disk. Reverted merge_pdfs to use pikepdf instead of PyPDF2. Uninstalled PyPDF2 and updated requirements.txt

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -67,53 +67,33 @@ def save_merged_pdf(directory, merged_pdf, total_amount_sum, file_prefix):
         new_file_name = f'{file_prefix}-{today}-{total_amount_sum}.pdf'
     else:  # LRD
         new_file_name = f'{today}-Loyalty.pdf'
-    output_dir = temp_dir[:-5]
-    output_path = os.path.join(output_dir, new_file_name)
+
+    output_path = os.path.join(directory, new_file_name)
     merged_pdf.save(output_path)
     merged_pdf.close()
-    merged_file_exists = check_file_exists(output_path)
-    if merged_file_exists:
-        if file_prefix == 'CCM':
-            print(f'EXXON CCM PDFs have been merged, renamed "{new_file_name}" and moved to: {output_path}\nDeleting temporary PDF files in {temp_dir}')
-        elif file_prefix == 'LRD':
-            print(f'Loyalty PDFs have been merged and saved as "{new_file_name}" to: {output_path}\nDeleting temporary PDF files in {temp_dir}')
-        temp_files_deleted = delete_pdf_files(temp_dir)
-        if temp_files_deleted:
-            print('Temporary PDF files have been deleted.')
-        else:
-            print('Temporary PDF files were not deleted.')
-    else:
-        print('Failed to save the merged PDF.')
 
-    merged_pdf.close()
+    # TODO: turn into helper for ensuring that merged pdfs exist in output directory
+    # merged_file_exists = check_file_exists(output_path)
+    # if merged_file_exists:
+    #     if file_prefix == 'CCM':
+    #         print(f'EXXON CCM PDFs have been merged, renamed "{new_file_name}" and moved to: {output_path}\nDeleting temporary PDF files in {temp_dir}')
+    #     elif file_prefix == 'LRD':
+    #         print(f'Loyalty PDFs have been merged and saved as "{new_file_name}" to: {output_path}\nDeleting temporary PDF files in {temp_dir}')
+    #     temp_files_deleted = delete_pdf_files(temp_dir)
+    #     if temp_files_deleted:
+    #         print('Temporary PDF files have been deleted.')
+    #     else:
+    #         print('Temporary PDF files were not deleted.')
+    # else:
+    #     print('Failed to save the merged PDF.')
+
     print(f'{file_prefix} PDFs have been merged, renamed "{new_file_name}" and saved to: {output_path}')
 
+def merge_rename_and_summate(directory):
+    pdf_data, total_amount_sum_ccm = extract_pdf_data(directory)
 
-def process_directory(directory):
-    pdf_data_ccm, total_amount_sum_ccm = extract_pdf_data(directory)
     merged_pdf_ccm = merge_pdfs(pdf_data_ccm)
     save_merged_pdf(directory, merged_pdf_ccm, total_amount_sum_ccm, 'CCM')
 
-    pdf_data_lrd, _ = extract_pdf_data(directory)
     merged_pdf_lrd = merge_pdfs(pdf_data_lrd)
     save_merged_pdf(directory, merged_pdf_lrd, None, 'LRD')
-
-
-# def merge_rename_and_summate(temp_dir):
-#     ccm_temp_dir = os.path.join(temp_dir, "CCM")
-#     lrd_temp_dir = os.path.join(temp_dir, "LRD")
-#
-#
-#     # Move the files to their respective directories
-#     for file in os.listdir(temp_dir):
-#         if file.startswith("CCM"):
-#             os.rename(os.path.join(temp_dir, file), os.path.join(ccm_temp_dir, file))
-#         elif file.startswith("LRD"):
-#             os.rename(os.path.join(temp_dir, file), os.path.join(lrd_temp_dir, file))
-#
-#     pdf_data_ccm, total_amount_sum_ccm = extract_pdf_data(ccm_temp_dir, 'CCM')
-#     merged_pdf_ccm = merge_pdfs(pdf_data_ccm)
-#     save_merged_pdf(ccm_temp_dir, merged_pdf_ccm, total_amount_sum_ccm, 'CCM')
-#
-#     pdf_data_lrd, _ = extract_pdf_data(lrd_temp_dir, 'LRD')
-#
