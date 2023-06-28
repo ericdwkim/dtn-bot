b4fc4e2a9b917dfef81779bee4979a0b5ed72682
ericdwkim <ericdwkim.io@gmail.com>
2023-06-28 13:28:51 -0500

    RFixed - total_amount in tuple

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -45,7 +45,7 @@ def extract_pdf_data(directory):
             regex_num_ccm, amount = extract_ccm_data(pdf_file)
             total_amount += amount
             total_amount = round(total_amount, 2)  # Round to two decimal places
-            pdf_data_ccm.append((regex_num_ccm, today, total_amount_rounded, os.path.join(directory, pdf_file)))
+            pdf_data_ccm.append((regex_num_ccm, today, total_amount, os.path.join(directory, pdf_file)))
         elif pdf_file.startswith('LRD'):
             regex_num_lrd, _ = extract_lrd_data(pdf_file)
             pdf_data_lrd.append((regex_num_lrd, today, _, os.path.join(directory, pdf_file)))
