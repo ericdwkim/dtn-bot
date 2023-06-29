5a15a950dfc6c3dda3ae525e4f70d88ea0ecb6c3
ericdwkim <ericdwkim.io@gmail.com>
2023-06-29 09:26:20 -0500

    Created mappings for prod in utils package and mappings for dev.

--- src/test/main.py
+++ src/test/main.py
@@ -4,6 +4,9 @@ from ..pages.dataconnectpage import DataConnectPage
 from utility import setup_driver, teardown_driver
 from utils.pdf_handler import process_pdfs, rename_and_move_pdf, get_full_path_to_dl_dir, rename_and_delete_pdf
 import subprocess
+from utils.mappings import (dest_dir_invoices, keyword_in_dl_file_name, download_dir, company_names,
+                      regex_patterns, company_name_to_subdir_full_path_mapping_fuel_drafts,
+                      company_name_to_subdir_full_path_mapping_credit_cards)
 
 # Run the shell script to delete PDF files from previous session
 subprocess.run(["../scripts/delete_pdf_files.sh"], shell=True)
@@ -16,42 +19,7 @@ def user_journey():
     driver = setup_driver()
 
 
-    # Directory paths
-    dest_dir_invoices = r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Invoices/5-May'
-    keyword_in_dl_file_name = 'messages'  # downloaded file is defaulted to filename `messages.pdf` on mac
-    download_dir = r'/Users/ekim/Downloads'
     full_path_to_downloaded_pdf = get_full_path_to_dl_dir(download_dir, keyword_in_dl_file_name)
-
-
-
-    company_names = ['VALERO', 'CONCORD FIRST DATA RETRIEVAL', 'EXXONMOBIL', 'U.S. OIL COMPANY', 'DK Trading & Supply',
-                     'CVR SUPPLY & TRADING, LLC']
-
-    regex_patterns = {'EFT-\s*\d+', 'CMB-\s*\d+', 'CCM-\s*\d+', 'RTV-\s*\d+', 'CBK-\s*\d+', 'LRD-\s*\d+'}
-
-    # Mapping for company name to Fuel Drafts subdir full path
-    company_name_to_subdir_full_path_mapping_fuel_drafts = {
-        'CVR SUPPLY & TRADING, LLC': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Drafts/CVR Supply & Trading 12351',
-
-        'EXXONMOBIL': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Drafts/EXXONMOBIL (10005)',
-
-        'U.S. OIL COMPANY': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Drafts/U S VENTURE - U S OIL COMPANY [12262]',
-
-        'VALERO': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Drafts/VALERO [10006]',
-
-        'DK Trading & Supply': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Fuel Drafts/DK TRADING [12293]'
-    }
-
-    # Mapping for company name to Credit Cards subdir full path
-    company_name_to_subdir_full_path_mapping_credit_cards = {
-
-        'VALERO': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Credit Cards/Valero (10006)',
-
-        'CONCORD FIRST DATA RETRIEVAL': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Credit Cards/First Data',
-
-        'EXXONMOBIL': r'/Users/ekim/workspace/txb/mock/K-Drive/DTN Reports/Credit Cards/EXXONMOBIL (10005)'
-
-    }
 
     try:
         # Visit site and login
