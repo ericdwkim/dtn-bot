11de4542a623a18845cafe2dec72d479510ced3e
ericdwkim <ericdwkim.io@gmail.com>
2023-07-01 18:08:47 -0500

    WIP - process_pages as instance method and process_pdfs as staticmethod that instantiates PdfProcessor prior to invoking process_pages. Need doc_type, company_id, and total_target_amt fetched dynamically with helper functions.

--- dev/test.py
+++ dev/test.py
@@ -2,9 +2,8 @@ from utils.mappings_refactored import (regex_patterns, doc_type_abbrv_to_doc_typ
 from utils.pdf_processor import PdfProcessor
 from utils.mappings_refactored import company_id_to_company_subdir_map
 
-dl_dir = PdfProcessor.download_dir
-file_path = dl_dir + '/etf_full.pdf'
 
 
+PdfProcessor.get_target_pdf_file_path()
 
-PdfProcessor.process_pdfs(file_path, regex_patterns, doc_type_abbrv_to_doc_type_subdir_map, company_id_to_company_subdir_map)
+PdfProcessor.process_pdfs(regex_patterns, doc_type_abbrv_to_doc_type_subdir_map, company_id_to_company_subdir_map)
