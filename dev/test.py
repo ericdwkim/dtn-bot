8e7a1f15156dfc29f0f2b900bed1f2e95f813fe4
ericdwkim <ericdwkim.io@gmail.com>
2023-06-30 22:36:22 -0500

    doc_type abbrv and company_id map imports for process_pdfs call in test.py

--- dev/test.py
+++ dev/test.py
@@ -1,7 +1,7 @@
 from utils.pdf_handler import process_pdfs, rename_and_move_pdf, get_full_path_to_dl_dir, rename_and_delete_pdf
 from utils.mappings import (dest_dir_invoices, keyword_in_dl_file_name, download_dir, company_names,
                       regex_patterns, company_name_to_subdir_full_path_mapping_fuel_drafts,
-                      company_name_to_subdir_full_path_mapping_credit_cards)
+                      company_name_to_subdir_full_path_mapping_credit_cards, doc_type_abbrv_to_doc_type_dir_map, company_id_to_company_subdir_map,)
 
 
 full_path_to_downloaded_pdf = r'/Users/ekim/Downloads/messages.pdf'
@@ -9,7 +9,7 @@ full_path_to_downloaded_pdf = r'/Users/ekim/Downloads/messages.pdf'
 
 # CCM, LRD files
 ccm_files_processed = process_pdfs(full_path_to_downloaded_pdf, company_name_to_subdir_full_path_mapping_credit_cards,
-                                   company_names, regex_patterns, post_processing=True)
+                                   company_names, regex_patterns, doc_type_abbrv_to_doc_type_dir_map, company_id_to_company_subdir_map, post_processing=True)
 if ccm_files_processed:
     # original_ccm_messages_pdf_is_deleted = rename_and_delete_pdf(full_path_to_downloaded_pdf)
     # print(f'Finished! original_ccm_messages_pdf_is_deleted: {original_ccm_messages_pdf_is_deleted}')
