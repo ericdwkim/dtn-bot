63b68617cf3d71b5eb16f8175df46fa429c892dd
ericdwkim <ericdwkim.io@gmail.com>
2023-07-07 17:14:54 -0500

    Passing Service object due to DeprecationWarning

--- utility.py
+++ utility.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 import undetected_chromedriver as uc
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
 
 def setup_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless=new')
     # options.add_argument('--start-maximized')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
     return driver
 
 def teardown_driver(driver):
