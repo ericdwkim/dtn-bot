55a11ce8725e41ca0de857e48b1f3fa69021d3e9
ericdwkim <ericdwkim.io@gmail.com>
2023-07-20 16:14:00 -0500

    Rename delete_pdf_files to delete_for_accpt.

--- src/test/config.py
+++ src/test/config.py
@@ -5,7 +5,7 @@ from subprocess import run
 def setup_config():
 
     # Run the shell script to delete PDF files from previous session
-    # run(["../scripts/delete_pdf_files.sh"], shell=True)
+    # run(["../scripts/delete_for_accpt_test.sh"], shell=True)
     print(f'===========================================================================================')
 
     # Set environmental variables
