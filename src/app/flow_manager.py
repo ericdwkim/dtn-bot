56567b2f203bd70ed03d4493112cd003198079dc
ericdwkim <ericdwkim.io@gmail.com>
2023-07-26 11:07:36 -0500

    Wrap start_flow in try-except. Successfully logs in and switches tab as expected.

--- src/app/flow_manager.py
+++ src/app/flow_manager.py
@@ -7,23 +7,28 @@ class FlowManager:
 
     # @dev: what all flows should do in sequence regardless of what flow number assuming they are independently ran
     def start_flow(self):
-        site_visited_and_logged_in = self.login_page_driver.visit_and_login()
-        # if not site_visited_and_logged_in:
-        #     raise RuntimeError('Something went wrong1')
+        try:
+            site_visited_and_logged_in = self.login_page_driver.visit_and_login()
+            if not site_visited_and_logged_in:
+                raise RuntimeError('site_visited_and_logged_in returned false')
+            else:
+                print(f'logged in successfully!')
 
-        tab_switched_to_data_connect = self.data_connect.switch_tab()
-        if not tab_switched_to_data_connect:
-            raise RuntimeError('Something went wrong2')
+            tab_switched_to_data_connect = self.data_connect.switch_tab()
+            if not tab_switched_to_data_connect:
+                raise RuntimeError('tab_switched_to_data_connect returned false')
 
-        # todo: not sure what changes will need to made, but it will be needed for all three flows, just different for third flow using `third_flow` bool
-        date_filter_set = self.data_connect.set_date_filter()
-        if not date_filter_set:
-            raise RuntimeError('Something went wrong3')
 
-        translated_set_to_no = self.data_connect.set_translated_filter_to_no()
-        if not translated_set_to_no:
-            raise RuntimeError('Something went wrong')
+            # todo: not sure what changes will need to made, but it will be needed for all three flows, just different for third flow using `third_flow` bool
+            date_filter_set = self.data_connect.set_date_filter()
+            if not date_filter_set:
+                raise RuntimeError('Something went wrong3')
 
+            translated_set_to_no = self.data_connect.set_translated_filter_to_no()
+            if not translated_set_to_no:
+                raise RuntimeError('Something went wrong')
+        except Exception as e:
+            print(f'An error occurred trying to start_flow: {e}')
 
     def end_flow(self):
         self.base_driver.teardown_driver()
