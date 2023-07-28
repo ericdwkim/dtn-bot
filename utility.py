090f0e69d5e6221aaa950cee73894e1e9032d7f4
ericdwkim <ericdwkim.io@gmail.com>
2023-07-28 12:52:58 -0500

    Acceptance Testing - Selenium Manager upgrades ChromeDriver. Deprecating ChromeDriverManger due to version 114 cap. Upgraded `selenium` to `4.10.0` to use latest Selenium Manager.

--- utility.py
+++ utility.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 def setup_driver():
@@ -8,9 +7,11 @@ def setup_driver():
     :return: `driver` of WebDriver class
     """
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless=new')
-    options.add_argument('--start-maximized')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options.add_argument('--headless=new')
+    # options.add_argument('--start-maximized')
+    # @dev: 7/28/23 - Selenium Manager (selenium==4.10.0) includes ChromeDriver upgrade. Deprecated ChromeDriverManager due to v114 cap
+    # https://stackoverflow.com/questions/76724939/there-is-no-such-driver-by-url-https-chromedriver-storage-googleapis-com-lates
+    driver = webdriver.Chrome(service=Service(executable_path='./chromedriver.exe'), options=options)
     return driver
 
 def teardown_driver(driver):
