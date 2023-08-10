362e83923a42128be37fcd4581ac7ae30a94c710
ericdwkim <ericdwkim.io@gmail.com>
2023-08-09 22:12:09 -0500

    Test Successful - `first_flow` downloads Invoices as expected.

--- src/app/run_flow.py
+++ src/app/run_flow.py
@@ -1,20 +1,19 @@
 from src.app.flow_manager import FlowManager
-from src.app.drivers import DataConnectDriver
-from src.pages import DataConnectPage
 import logging
 
 def first_flow():
     flow_manager = FlowManager()
-    data_connect_page = DataConnectDriver()
     logging.info(f'\n---------------------------\nInitiating First Flow\n---------------------------\n')
     try:
         flow_manager.start_flow()
         # first flow specific logic
-        group_filter_set_to_invoice = data_connect_page.set_group_filter_to_invoice()
+        group_filter_set_to_invoice =  flow_manager.data_connect_driver.set_group_filter_to_invoice()
+
         if not group_filter_set_to_invoice:
             logging.error('Could not set group filter to invoice')
 
-
+        else:
+            invoices_downloaded = flow_manager.data_connect_driver.data_connect_page.check_all_then_click_print()
 
     finally:
         flow_manager.end_flow()
