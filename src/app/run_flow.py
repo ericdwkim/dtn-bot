89b1a438d10348adf2538a3bff8ccde1e8a5d464
ericdwkim <ericdwkim.io@gmail.com>
2023-08-09 23:57:41 -0500

    Added `processor` instance to invoke `rename_and_move_or_overwrite_invoices_pdf` in `first_flow`

--- src/app/run_flow.py
+++ src/app/run_flow.py
@@ -1,37 +1,48 @@
 from src.app.flow_manager import FlowManager
+from utils.pdf_processor import PdfProcessor
 import logging
 
 def first_flow():
     flow_manager = FlowManager()
+    processor = PdfProcessor()
     logging.info(f'\n---------------------------\nInitiating First Flow\n---------------------------\n')
     try:
         flow_manager.start_flow()
-        # first flow specific logic
+
         group_filter_set_to_invoice =  flow_manager.data_connect_driver.set_group_filter_to_invoice()
 
         if not group_filter_set_to_invoice:
             logging.error('Could not set group filter to invoice')
 
+        invoices_downloaded = flow_manager.data_connect_driver.data_connect_page.check_all_then_click_print()
+
+        if not invoices_downloaded:
+            logging.error('Could not download Invoices')
+
         else:
-            invoices_downloaded = flow_manager.data_connect_driver.data_connect_page.check_all_then_click_print()
+            invoices_renamed_and_filed_away = processor.rename_and_move_or_overwrite_invoices_pdf()
+
+            if not invoices_renamed_and_filed_away:
+                logging.error('Could not rename and file away invoices. Does the Invoices PDF exist?')
 
     finally:
         flow_manager.end_flow()
-
 
 first_flow()
 
 # def second_flow():
 #     flow_manager = FlowManager()
+#     logging.info(f'\n---------------------------\nInitiating Second Flow\n---------------------------\n')
 #
 #     try:
 #         flow_manager.start_flow()
-#         # ... Rest of second_flow logic ...
+#
+#
 #     finally:
 #         flow_manager.end_flow()
-#
-#
-#
+
+
+
 # def third_flow():
 #
 #     # third flow specific logic
