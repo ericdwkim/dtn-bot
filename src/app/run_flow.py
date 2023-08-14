0182280250fdbb47a805ce963413fba35edcb797
ericdwkim <ericdwkim.io@gmail.com>
2023-08-14 18:17:55 -0500

    Added command-line arg flags for headless mode or to start Chrome maximized.

--- src/app/run_flow.py
+++ src/app/run_flow.py
@@ -1,12 +1,11 @@
+import argparse
+import logging
 from src.app.flow_manager import FlowManager
 from utils.pdf_processor import PdfProcessor
-import logging
-
-flow_manager = FlowManager()
-processor = PdfProcessor()
 
 
-def first_flow():
+
+def first_flow(flow_manager, processor):
     logging.info(f'\n---------------------------\nInitiating First Flow\n---------------------------\n')
     try:
         flow_manager.start_flow()
@@ -38,7 +37,7 @@ def first_flow():
 
 
 
-def second_flow():
+def second_flow(flow_manager, processor):
     logging.info(f'\n---------------------------\nInitiating Second Flow\n---------------------------\n')
 
     try:
@@ -48,22 +47,48 @@ def second_flow():
         if not group_filter_set_to_draft_notice:
             logging.error('Could not set group filter to Draft Notice during second_flow')
 
+        draft_notices_processed_and_filed = processor.process_pages()
+
+        if not draft_notices_processed_and_filed:
+            logging.error('Could not download Draft Notices')
+
+
 
     finally:
+        # TODO:
+        #processor.rename_and_delete_pdf()
         flow_manager.end_flow()
+        logging.info('END OF SECOND__FLOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 
 
-# def third_flow():
+# def third_flow(flow_manager, processor):
 #
 #     # third flow specific logic
 #     pass
 #
 #
 #
-def run_flows():
-    first_flow()
-    pass
+def run_flows(flow_manager, processor, args):
+    if not args.skipFlow1:
+        first_flow(flow_manager, processor)
+    if not args.skipFlow2:
+        second_flow(flow_manager, processor)
+    if not args.skipFlow3:
+        third_flow(flow_manager, processor)
 
 
-run_flows()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DTN Bot V2')
+    parser.add_argument('--headless', required=False, action='store_true', help='Run in headless mode')
+    parser.add_argument('--skipFlow1', required=False, action='store_true', help='Skip the first flow')
+    parser.add_argument('--skipFlow2', required=False, action='store_true', help='Skip the second flow')
+    parser.add_argument('--skipFlow3', required=False, action='store_true', help='Skip the third flow')
+    args = parser.parse.args()
+
+    flow_manager = FlowManager(headless=args.headless)
+    processor = PdfProcessor()
+
+    run_flows(flow_manager, processor, args)
+    logging.info(f'\n---------------------------\nCommencing All Flows\n---------------------------\n')
