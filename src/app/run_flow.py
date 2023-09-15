3974ff6919fb23d48e4fd06eaf4e71ede33f876a
ericdwkim <ericdwkim.io@gmail.com>
2023-09-15 14:15:14 -0400

    removed additional `check_all_then_click_print` calls in flow funcs. removed `finally` blocks with flow specific logic prior to `end_flow`. converted `flows_to_run` to list of tups to conditionally invoke `rename_and_delete_pdf` prior to `end_flow` -- requires testing

--- src/app/run_flow.py
+++ src/app/run_flow.py
@@ -5,7 +5,7 @@ from src.app.flow_manager import FlowManager
 from utils.pdf_processor import PdfProcessor
 
 
-# todo: keeps trying to save in working dir `src` so it creates new Fuel Invoices/yyyy/mm
+# todo: keeps trying to save in working dir `src` so it creates new Fuel Invoices/yyyy/mm; need it to save it in downloads dir as from and overwrite/save to target dir
 def first_flow(flow_manager, processor):
     logging.info(f'\n---------------------------\nInitiating First Flow\n---------------------------\n')
     try:
@@ -15,11 +15,6 @@ def first_flow(flow_manager, processor):
 
         if not group_filter_set_to_invoice:
             logging.error('Could not set group filter to invoice')
-
-        invoices_downloaded = flow_manager.data_connect_driver.data_connect_page.check_all_then_click_print()
-
-        if not invoices_downloaded:
-            logging.error('Could not download Invoices')
 
         invoices_renamed_and_filed_away = processor.rename_and_move_or_overwrite_invoices_pdf()
 
@@ -37,31 +32,16 @@ def first_flow(flow_manager, processor):
 
 
 
-    # finally:
-    #     invoices_renamed_and_filed_away = processor.rename_and_move_or_overwrite_invoices_pdf()
-    #
-    #     if not invoices_renamed_and_filed_away:
-    #         logging.error('Could not rename and file away invoices. Does the Invoices PDF exist?')
-    #
-    #     elif invoices_renamed_and_filed_away and processor.is_last_day_of_month():
-    #         processor.month_and_year_handler(first_flow=True)
-    #
-    #     flow_manager.end_flow()
-    #
-    #     logging.info(f'\n---------------------------\nCommencing First Flow\n---------------------------\n')
-
-
-
 def second_flow(flow_manager, processor):
     logging.info(f'\n---------------------------\nInitiating Second Flow\n---------------------------\n')
 
     try:
-        # flow_manager.start_flow()
         group_filter_set_to_draft_notice = flow_manager.data_connect_driver.set_group_filter_to_draft_notice()
         logging.info(f'group_filter_set_to_draft_notice: {group_filter_set_to_draft_notice}')
 
         if not group_filter_set_to_draft_notice:
             logging.error('Could not set group filter to Draft Notice during second_flow')
+
 
         draft_notices_processed_and_filed = processor.process_pages()
 
@@ -76,36 +56,29 @@ def second_flow(flow_manager, processor):
         logging.info(f'an error: {e}')
 
 
-
-    # finally:
-    #     # TODO:
-    #     #processor.rename_and_delete_pdf()
-    #     flow_manager.end_flow()
-    #     logging.info('END OF SECOND__FLOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
-
-
-def third_flow(flow_manager, processor):
-    logging.info('running third flow....')
+# todo
+# def third_flow(flow_manager, processor):
+#     logging.info('running third flow....')
 
     # third flow specific logic
-    pass
+    # pass
 
 
-# @dev: call w/o args if wish to run all three flows in sequential order using the same ChromeDriver instance todo: will need to refactor flow funcs to conditionally `start_flow` and `end_flow` depending on whether it is to be ran in sequential order (only one `start_flow` and `end_flow` necessary OR individually (currently as is)
+# @dev: call w/o args if wish to run all three flows in sequential order using the same ChromeDriver instance
 def run_flows(flow_manager, processor, flows):
     # Setup session
     flow_manager.start_flow()
 
-    for flow in flows:
-        logging.info(f'Running flow: {flow}')
-        flow(flow_manager, processor)   # Execute all flow(s)
+    for flow_func, flow_name in flows:
+        logging.info(f'Running flow: {flow_name}')
+        flow_func(flow_manager, processor)   # Execute all flow(s)
+
+        # Conditional logic based on the flow name
+        if flow_name in ['second_flow', 'third_flow']:
+            processor.rename_and_delete_pdf()
 
     # Terminate session
     flow_manager.end_flow()
-
-
-#todo: if wish to call individual flows, will need to ensure each flow calls its own `start_flow` and `end_flow`
 
 
 
@@ -122,11 +95,11 @@ if __name__ == '__main__':
 
     flows_to_run = []
     if not args.skipFlow1:
-        flows_to_run.append(first_flow)
+        flows_to_run.append((first_flow, 'first_flow'))
     if not args.skipFlow2:
-        flows_to_run.append(second_flow)
+        flows_to_run.append((second_flow, 'second_flow'))
     if not args.skipFlow3:
-        flows_to_run.append(third_flow)
+        flows_to_run.append((third_flow, 'third_flow'))
 
     # Delete all PDFs in Downloads directory
     # run(["../scripts/clean_slate.sh"], shell=True)
