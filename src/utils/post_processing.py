75b535f774d2f70997b4526914a4abf029991166
ericdwkim <ericdwkim.io@gmail.com>
2023-10-19 01:20:27 -0500

    wip: post_processing updates with logging; `merge_rename_and_summate` fixes

--- src/utils/post_processing.py
+++ src/utils/post_processing.py
@@ -1,5 +1,6 @@
 import os
 import re
+import logging
 from datetime import datetime
 import pikepdf
 import shutil
@@ -28,6 +29,10 @@ class PDFPostProcessor:
                 self.new_pdf.pages.extend(pdf.pages)
             except pikepdf.PdfError:
                 return False
+        merged_pdf = self.new_pdf
+        return merged_pdf
+
+
 
     def save_merged_pdf(self, file_prefix, merged_pdf, total_amount_sum, company_id):
         """
@@ -54,6 +59,7 @@ class PDFPostProcessor:
         except Exception:
             return False
 
+    # todo: clean and refactor w/ error handling and return bool
     def merge_rename_and_summate(self, company_dir):
         """
         Main post-processing wrapper function. Accounts for end of month operations if last day of the month.
@@ -61,22 +67,37 @@ class PDFPostProcessor:
         :return: None
         """
         pdf_data_ccm, total_amount_sum_ccm, pdf_data_lrd = self.extraction_handler.extract_pdf_data(company_dir)
-        print(
+        logging.info(
             f'********************* pdf_data_ccm: {pdf_data_ccm}\n total_amount_sum_ccm: {total_amount_sum_ccm}\n *********** pdf_data_lrd {pdf_data_lrd}  ')
 
-        merged_pdf_ccm = merge_pdfs(pdf_data_ccm)
-        merged_ccm_pdf_is_saved = save_merged_pdf('CCM', merged_pdf_ccm, total_amount_sum_ccm, '10005')
-        print(f'merged_pdf_ccm / merged_ccm_pdf_is_saved: {merged_pdf_ccm} / {merged_ccm_pdf_is_saved}')
+        merged_pdf_ccm = self.merge_pdfs(pdf_data_ccm)
+        merged_ccm_pdf_is_saved = self.save_merged_pdf('CCM', merged_pdf_ccm, total_amount_sum_ccm, '10005')
+        logging.info(f'merged_pdf_ccm / merged_ccm_pdf_is_saved: {merged_pdf_ccm} / {merged_ccm_pdf_is_saved}')
         # Clean up pre-merged PDFs in EXXON company_dir; loops through file_path (4th elem in tuple) to delete
         self.file_handler.cleanup_files(pdf_data_ccm)
         # PDFs were merged, saved w/ new filename. If it is currently the last day of the month, then perform end of month filesystem management
         if merged_pdf_ccm and merged_ccm_pdf_is_saved and self.file_handler.is_last_day_of_month():
             self.file_handler.end_of_month_operations(company_dir)
 
-        merged_pdf_lrd = merge_pdfs(pdf_data_lrd)
-        merged_lrd_pdf_is_saved = save_merged_pdf('LRD', merged_pdf_lrd, None, '10005')
+        elif merged_ccm_pdf_is_saved and merged_pdf_ccm:
+            logging.info(f'merged_ccm_pdf_is_saved: {merged_ccm_pdf_is_saved} | merged_pdf_ccm: {merged_pdf_ccm}')
+            return True
+
+        else:
+            return False
+
+
+        merged_pdf_lrd = self.merge_pdfs(pdf_data_lrd)
+        merged_lrd_pdf_is_saved = self.save_merged_pdf('LRD', merged_pdf_lrd, None, '10005')
         # Clean up pre-merged PDFs in EXXON company_dir; loops through file_path (4th elem in tuple) to delete
         self.file_handler.cleanup_files(pdf_data_lrd)
         # PDFs were merged, saved w/ new filename. If it is currently the last day of the month, then perform end of month filesystem management
         if merged_pdf_lrd and merged_lrd_pdf_is_saved and self.file_handler.is_last_day_of_month():
             self.file_handler.end_of_month_operations(company_dir)
+
+        elif merged_pdf_lrd and merged_lrd_pdf_is_saved:
+            logging.info(f'merged_lrd_pdf_is_saved: {merged_lrd_pdf_is_saved} | merged_pdf_lrd: {merged_pdf_lrd}')
+            return True
+
+        else:
+            return False
